3cd06af59b6945c73d74061014165341e34167d1
海朋冯 <haipengfeng@haipengfengs-Mac-mini.local>
2023-04-26 20:34:34 +0800

    update

--- 9.react的使用/react-demo/src/App.js
+++ 9.react的使用/react-demo/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import BaseUse from './components/baseUse';
 import AdvancedUse from './components/advancedUse';
 // import ReduxUse from './components/reduxUse';
 // import TodoList from './components/TodoLIst'
 
 function App() {
+  const [val, setVal] = useState(0);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log('hello');
+    }, 2000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div>
+      <input onChange={() => setVal(val + 1)} /> {val}
       {/* <BaseUse /> */}
-      <AdvancedUse />
+      {/* <AdvancedUse /> */}
       {/* <ReduxUse/> */}
       {/* <TodoList/> */}
     </div>
